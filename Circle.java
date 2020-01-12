339eee6c19e2e74f7b6f3f8397523d0f9118b016
Megha Khatiwala <khatiwalamegha@gmail.com>
2020-01-12 11:31:09 -0600

    final commit

---
+++ Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import static java.lang.Math.*;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return PI * radius * radius;
+    }
+}
